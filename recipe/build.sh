98a944b776a42905ff45f775c2fc7ea137dd71d5
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-05-26 13:04:05 +0200

    use $SHLIB_EXT

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,17 +21,17 @@ if [ ${PY3K} = 1 ]; then
 	cmake $args \
 	      -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
 	      -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m \
-	      -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m.so \
+	      -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m.$SHLIB_EXT \
 	      ..
 else 
 	cmake $args \
 	      -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
 	      -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER} \
-	      -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.so \
+	      -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.$SHLIB_EXT \
 	      ..
 fi;
 make 
-cp lib/ovito/*.so ${PREFIX}/lib
+cp lib/ovito/*.$SHLIB_EXT ${PREFIX}/lib
 cp -r lib/ovito/plugins/python/ovito ${SP_DIR}
 mkdir -p ${PREFIX}/lib/ovito/plugins
 cp lib/ovito/plugins/*.so ${PREFIX}/lib/ovito/plugins
