d90915d838f1e78ce4c638356abc2bfd2481bd1c
Eric Prestat <eric.prestat@gmail.com>
2020-10-27 14:01:06 +0000

    Update to last version, enable GUI, disable building python package since the python source code is not in the repository.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
 
-args="-DOVITO_BUILD_PLUGIN_MESH=ON \
-      -DOVITO_BUILD_PLUGIN_PARTICLES=ON \
-      -DOVITO_BUILD_PLUGIN_TACHYON=OFF \
-      -DOVITO_BUILD_PLUGIN_CRYSTALANALYSIS=OFF \
-      -DOVITO_BUILD_PLUGIN_NETCDFPLUGIN=OFF \
-      -DOVITO_BUILD_PLUGIN_POVRAY=OFF \
-      -DOVITO_BUILD_PLUGIN_OPENBABELPLUGIN=OFF \
-      -DOVITO_BUILD_PLUGIN_VR=OFF \
-      -DOVITO_BUILD_PLUGIN_CORRELATION=ON \
-      -DOVITO_BUILD_PLUGIN_VOROTOP=ON \
-      -DOVITO_BUILD_GUI=OFF \
-      -DOVITO_BUILD_DOCUMENTATION=OFF \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=${PREFIX}" 
-
 mkdir build
 cd build
-if [ ${PY3K} = 1 ]; then 
-	cmake $args \
-	      -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
-	      -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m \
-	      -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT} \
-	      ..
-else 
-	cmake $args \
-	      -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
-	      -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER} \
-	      -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}${SHLIB_EXT} \
-	      ..
-fi;
-make 
-cp lib/ovito/*${SHLIB_EXT} ${PREFIX}/lib
-cp -r lib/ovito/plugins/python/ovito ${SP_DIR}
-mkdir -p ${PREFIX}/lib/ovito/plugins
-cp lib/ovito/plugins/*${SHLIB_EXT} ${PREFIX}/lib/ovito/plugins
-sed -i 's\/../../..\/../../../../ovito/plugins\g' ${SP_DIR}/ovito/plugins/__init__.py
+
+cmake -DOVITO_BUILD_DOCUMENTATION=OFF \
+      -DOVITO_BUILD_GUI=ON \
+      -DOVITO_BUILD_MONOLITHIC=OFF \
+      -DOVITO_REDISTRIBUTABLE_PACKAGE=OFF \
+      -DOVITO_USE_PRECOMPILED_HEADERS=ON \
+      -DOVITO_BUILD_PYTHON_PACKAGE=OFF \
+      -DOVITO_BUILD_CONDA=ON \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      ..
+
+make -j ${CPU_COUNT}
+make install
