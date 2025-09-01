39ba643600bc596bf98026b2b3fd9168689a1566
Alexander Stukowski <stukowski@ovito.org>
2025-09-01 11:54:13 +0200

    MNT: Update build configuration to use Ninja and enable SSH client support on Windows; update zstd version to 1.5.7

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,8 +6,8 @@ if not exist %MENU_DIR% mkdir %MENU_DIR%
 copy %RECIPE_DIR%\ovito.ico %MENU_DIR%
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\ovito.json
 
-:: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" ^
+:: Configure
+cmake -G Ninja ^
       -D Python3_ROOT_DIR=%BUILD_PREFIX% ^
       -D Python3_FIND_STRATEGY=LOCATION ^
       -D Python3_FIND_VIRTUALENV=ONLY ^
@@ -15,7 +15,7 @@ cmake -G "NMake Makefiles" ^
       -D OVITO_BUILD_DOCUMENTATION=ON ^
       -D OVITO_BUILD_MONOLITHIC=OFF ^
       -D OVITO_USE_PRECOMPILED_HEADERS=ON ^
-      -D OVITO_BUILD_SSH_CLIENT=OFF ^
+      -D OVITO_BUILD_SSH_CLIENT=ON ^
       -D OVITO_BUILD_CONDA=ON ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -23,8 +23,8 @@ cmake -G "NMake Makefiles" ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
-nmake
+cmake --build . --parallel
 if errorlevel 1 exit 1
 
-nmake install
+cmake --install .
 if errorlevel 1 exit 1
